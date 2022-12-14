08fc5f26164109fe38688ee6f1ef430ae902fdf0
Praveen <spraveenitpro@gmail.com>
2022-12-13 23:51:43 -0800

    Bitcoin wallet in react style edit

--- plebwalletreact/src/App.jsx
+++ plebwalletreact/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
     const [price, setPrice] = useState(null)
     const [balance, setBalance] = useState(null)
+    const [transactions, setTransactions] = useState([])
     const getPrice = () => {
         // Axios is a library that makes it easy to make http requests
         // After we make a request, we can use the .then() method to handle the response asynchronously
@@ -28,7 +29,7 @@ function App() {
     const getWalletBalance = () => {
         // ToDo: Lookup how to move the X-API-Key to a .env file to keep it secret for when we push to Github
         const headers = {
-            'X-Api-Key': 'e19c6f42051f488dbd582a4191ecafc2',
+            'X-Api-Key': import.meta.env.VITE_TEST_VAR,
         }
         axios
             .get('https://legend.lnbits.com/api/v1/wallet', { headers })
@@ -39,9 +40,23 @@ function App() {
             .catch((err) => console.log(err))
     }
 
+    const getTransactions = () => {
+        const headers = {
+            'X-Api-Key': import.meta.env.VITE_TEST_VAR,
+        }
+        axios
+            .get('https://legend.lnbits.com/api/v1/payments', { headers })
+            .then((res) => {
+                setTransactions(res.data)
+            })
+            .catch((err) => console.log(err))
+    }
+
     useEffect(() => {
         getPrice()
         getWalletBalance()
+        getTransactions()
+        console.log(import.meta.env.VITE_TEST_VAR)
     }, [])
 
     useEffect(() => {
@@ -71,7 +86,7 @@ function App() {
             </div>
             <div className="row">
                 <div className="row-item">
-                    <Transactions />
+                    <Transactions transactions={transactions} />
                 </div>
                 <div className="row-item">
                     {/* <Chart chartData={chartData} /> */}
--- plebwalletreact/src/components/Transactions.jsx
+++ plebwalletreact/src/components/Transactions.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
 import './Transactions.css'
-export const Transactions = () => {
-    const transactions = [
-        {
-            checking_id: 1,
-            amount: 1000,
-            time: 1669064951,
-            bolt11: 'lnbc20n1p3h24fhsp590ccjwy...',
-        },
-        {
-            checking_id: 2,
-            amount: 2000,
-            time: 1668035952,
-            bolt11: 'lnbc980n1p3h24fhsp5cpcjdw...',
-        },
-    ]
-
+export const Transactions = ({ transactions }) => {
     const parseTx = (tx) => {
         // turn unix timestamp into a date
         // Todo: format date further to include hours, minutes, and seconds
         const date = new Date(tx.time * 1000)
         const formattedDate = date.toLocaleDateString('en-US')
-        return (
-            <div key={tx.checking_id} className="tx-item">
-                <p>Received from {tx.bolt11.substring(0, 25)}...</p>
-                <p>+{tx.amount / 1000} sats</p>
-                <p className="transaction-date">{formattedDate}</p>
-            </div>
-        )
+        // ToDo: Handle pending payments since we are currently ignoring them and not displaying them on our past transactions list
+        if (tx.pending) return null
+        if (tx.amount > 0) {
+            return (
+                <div key={tx.checking_id} className="tx-item">
+                    <p>Received from {tx.bolt11.substring(0, 25)}...</p>
+                    <p>+{tx.amount / 1000} sats</p>
+                    <p className="transaction-date">{formattedDate}</p>
+                </div>
+            )
+        }
+        if (tx.amount < 0) {
+            return (
+                <div
+                    id={tx.checking_id}
+                    key={tx.checking_id}
+                    className="tx-item"
+                >
+                    <p>Sent with {tx.bolt11.substring(0, 25)}...</p>
+                    <p className="tx-amount">{tx.amount / 1000} sats</p>
+                    <p className="transaction-date">{formattedDate}</p>
+                </div>
+            )
+        }
     }
 
     return (
